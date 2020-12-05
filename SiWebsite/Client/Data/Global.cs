c1f894b058ce138c0471e454dc3632854b57c2b7
Stuart Aitken <s.aitken88@gmail.com>
2020-12-05 13:19:06 +0000

    Add project files.

---
+++ SiWebsite/Client/Data/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiWebsite.Data
+{
+    public static class Global
+    {
+        public static readonly string SiteName = "Daleside Pest Control";
+        public static readonly string Slogan = "Local pest control for you";
+        public static readonly string ContactNum = "07875 687 790";
+
+        public static string[] PestsList =
+        {
+            "Rabbits", 
+            "Rats", 
+            "Moles", 
+            "Squirrels", 
+            //"Pigeons", 
+            //"Corvids", 
+            "Mink",
+        };
+
+        public static string selectedPestItem = "";
+
+        public static void SetSelectedPestItem(string pest)
+        {
+            selectedPestItem = pest;
+        }
+
+        public static int HeaderHeight = 60;
+        public static int FooterHeight = 100;
+        
+    }
+}
---
+++ SiWebsite/Client/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+
+namespace SiWebsite.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services
+                .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons()
+                .AddSingleton(new HttpClient
+                {
+                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+                });
+
+            var host = builder.Build();
+
+            host.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
