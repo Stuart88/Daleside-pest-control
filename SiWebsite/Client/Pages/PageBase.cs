79b3b24a9328ce283f960bb2176e0aa310261c7a
Stuart Aitken <s.aitken88@gmail.com>
2021-08-09 21:16:35 +0700

    Improve pest icons layout on mobile screen

---
+++ SiWebsite/Client/Pages/PageBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiWebsite.Client.Pages
+{
+    public class PageBase : LayoutComponentBase
+    {
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        public async Task<bool> IsMobileCheck()
+        {
+           return await JS.InvokeAsync<bool>("isMobile");
+        }
+
+        public bool IsMobile { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                this.IsMobile = await this.IsMobileCheck();
+                this.StateHasChanged();
+            }
+        }
+
+    }
+}
---
+++ SiWebsite/Client/wwwroot/scripts/scripts.js
@@ -0,0 +1,6 @@
+﻿
+
+window.isMobile = () => {
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
+}
+
